2835c1b20430de1b40f03824857a2a33195b12e0
NebulousHaze <78150190+NebulousHaze@users.noreply.github.com>
2021-01-28 18:21:40 +0100

    Add existing file

---
+++ JSON-Coord-Generator/JSON-Coord-Generator/frmMain.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace JSON_Coord_Generator
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenu));
+            this.txbX = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbZ = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbY = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txbX
+            // 
+            this.txbX.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbX.Location = new System.Drawing.Point(185, 351);
+            this.txbX.Name = "txbX";
+            this.txbX.Size = new System.Drawing.Size(281, 62);
+            this.txbX.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(308, 311);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(949, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 37);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Z";
+            // 
+            // txbZ
+            // 
+            this.txbZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbZ.Location = new System.Drawing.Point(826, 351);
+            this.txbZ.Name = "txbZ";
+            this.txbZ.Size = new System.Drawing.Size(281, 62);
+            this.txbZ.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(628, 311);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 37);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Y";
+            // 
+            // txbY
+            // 
+            this.txbY.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbY.Location = new System.Drawing.Point(505, 351);
+            this.txbY.Name = "txbY";
+            this.txbY.Size = new System.Drawing.Size(281, 62);
+            this.txbY.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(534, 488);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(224, 104);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Generate Grid";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(565, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 37);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Filename";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(414, 180);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(464, 62);
+            this.textBox2.TabIndex = 0;
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.ClientSize = new System.Drawing.Size(1293, 725);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbY);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txbZ);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbX);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jonathan\'s Coord Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbZ;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbY;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ JSON-Coord-Generator/JSON-Coord-Generator/frmMain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Media;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace JSON_Coord_Generator
+{
+
+    public partial class frmMenu : Form
+    {
+        public static int DefinitiveCoordX;     //used to make the right number of squares      
+        public static int DefinitiveCoordY;     // "" ""
+        public static int DefinitiveCoordZ;     // "" ""
+        public static string name;              //filename
+        byte bytCoordsGood;                     //decide whether to start program or not based on conditions passes
+
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// check if the value in XYZ textboxes are good and start program if good
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txbX.Text, out int xcord) && xcord > 0)
+            {
+                bytCoordsGood++;
+            }
+            if (int.TryParse(txbY.Text, out int ycord) && ycord > 0)
+            {
+                bytCoordsGood++;
+            }
+            if (int.TryParse(txbZ.Text, out int zcord) && zcord > 0)
+            {
+                bytCoordsGood++;
+            }
+            if (bytCoordsGood == 3)
+            {
+                name = textBox2.Text;
+                btnStart.Text = "Loading...";
+                DefinitiveCoordX = xcord;
+                DefinitiveCoordY = ycord;
+                DefinitiveCoordZ = zcord;
+                Form frmStartGrid = new frmGrid();
+                this.Hide();
+                frmStartGrid.Show();
+            }
+            else
+            {
+                bytCoordsGood = 0;
+            }
+        }
+    }
+}
